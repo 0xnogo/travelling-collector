ae6f22a44dfc60b699f0f73804821cd800783402
nogo <0xnogo@proton.me>
2022-11-04 20:03:10 +0100

    balance filtering

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::{error::Error, str::FromStr};
+use std::error::Error;
 
 use ethers::prelude::*;
 use futures::future;
 
+const BALANCE_THRESHOLD: u8 = 0;
+
 // listening to block creation
-pub async fn monitoring_blocks(ws_provider: Provider<Ws>) -> Option<()> {
+pub async fn monitoring_blocks(ws_provider: &Provider<Ws>) -> Option<()> {
     let mut block_stream = ws_provider.subscribe_blocks().await.unwrap();
 
     println!("---------- MONITORING NEW BLOCKS ----------");
@@ -23,18 +25,65 @@ pub async fn monitoring_blocks(ws_provider: Provider<Ws>) -> Option<()> {
         .unwrap();
 
         // filter only creation tx (to is null)
-        let finalzer: Vec<Transaction> = transactions
+        let contract_creation_transactions: Vec<Transaction> = transactions
             .into_iter()
             .filter(|t| t.is_some())
             .map(|t| t.unwrap())
             .filter(|t| t.to.is_none())
             .collect();
 
-        println!("Number of smart contract creation {}", finalzer.len());
-        println!("Txs: {:?}", finalzer);
+        println!(
+            "Number of smart contract creation {}",
+            contract_creation_transactions.len()
+        );
+
+        // get the contract address
+        let tx_receipts = future::try_join_all(
+            contract_creation_transactions
+                .iter()
+                .map(|tx| ws_provider.get_transaction_receipt(tx.hash)),
+        )
+        .await
+        .unwrap();
+
+        let contract_address: Vec<H160> = tx_receipts
+            .into_iter()
+            .filter(|tx_receipts| tx_receipts.is_some())
+            .map(|tx_receipts| tx_receipts.unwrap().contract_address.unwrap())
+            .collect();
+
+        println!("Smart contracts: {:?}", contract_address);
+        let balances = future::try_join_all(
+            contract_address
+                .iter()
+                .map(|addr| get_balance_address(&ws_provider, *addr, block.hash.unwrap())),
+        )
+        .await
+        .unwrap();
+
+        println!("Balances: {:?}", balances);
+
+        let filtered_balances: Vec<(H160, U256)> = balances
+            .into_iter()
+            .filter(|(_, balance)| balance > &U256::from(BALANCE_THRESHOLD))
+            .collect();
+
+        println!("Balances with balance > 0 eth: {:?}", filtered_balances);
     }
 
     Some(())
+}
+
+// get balance of contract
+pub async fn get_balance_address(
+    ws_provider: &Provider<Ws>,
+    from: H160,
+    block: H256,
+) -> Result<(H160, U256), Box<dyn Error>> {
+    let block_id = BlockId::Hash(block);
+    let balance = ws_provider.get_balance(from, Some(block_id)).await.unwrap();
+
+    Ok((from, balance))
 }
 
 // getting a list of all tx from a specific block
@@ -46,17 +95,14 @@ pub async fn get_all_tx_from_block(
     Ok(block.transactions)
 }
 
-// get balance of contract
-
 #[tokio::main]
 async fn main() {
-    let zero_address = H160::from_str("0x0000000000000000000000000000000000000000");
-    println!("{:?}", zero_address);
     let ws_provider =
         Provider::<Ws>::connect("wss://mainnet.infura.io/ws/v3/c60b0bb42f8a4c6481ecd229eddaca27")
             .await
             .unwrap();
+
     // allow going backwards
     // allow chose from where to start
-    monitoring_blocks(ws_provider).await;
+    monitoring_blocks(&ws_provider).await;
 }
