ebd2a82c819e3b1229d2eb3c9286694f9352f4d6
nogo <0xnogo@proton.me>
2022-11-05 11:32:16 +0100

    refactor the monitoring_blocks

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,11 @@ pub async fn monitoring_blocks(ws_provider: &Provider<Ws>) -> Option<()> {
             .await
             .unwrap();
 
-        let transactions = future::try_join_all(
-            transactions_fetched
-                .iter()
-                .map(|tx_hash| ws_provider.get_transaction(*tx_hash)),
-        )
-        .await
-        .unwrap();
+        let transactions = get_transactions(&ws_provider, transactions_fetched).await;
 
         // filter only creation tx (to is null)
-        let contract_creation_transactions: Vec<Transaction> = transactions
-            .into_iter()
-            .filter(|t| t.is_some())
-            .map(|t| t.unwrap())
-            .filter(|t| t.to.is_none())
-            .collect();
+        let contract_creation_transactions: Vec<Transaction> =
+            filter_contract_creation(transactions);
 
         println!(
             "Number of smart contract creation {}",
@@ -38,37 +28,18 @@ pub async fn monitoring_blocks(ws_provider: &Provider<Ws>) -> Option<()> {
         );
 
         // get the contract address
-        let tx_receipts = future::try_join_all(
-            contract_creation_transactions
-                .iter()
-                .map(|tx| ws_provider.get_transaction_receipt(tx.hash)),
-        )
-        .await
-        .unwrap();
-
-        let contract_address: Vec<H160> = tx_receipts
-            .into_iter()
-            .filter(|tx_receipts| tx_receipts.is_some())
-            .map(|tx_receipts| tx_receipts.unwrap().contract_address.unwrap())
-            .collect();
+        let contract_address =
+            get_contract_addresses(ws_provider, contract_creation_transactions).await;
 
         println!("Smart contracts: {:?}", contract_address);
-        let balances = future::try_join_all(
-            contract_address
-                .iter()
-                .map(|addr| get_balance_address(&ws_provider, *addr, block.hash.unwrap())),
-        )
-        .await
-        .unwrap();
+        let balances = get_balances(&ws_provider, contract_address, block.hash.unwrap()).await;
 
         println!("Balances: {:?}", balances);
 
-        let filtered_balances: Vec<(H160, U256)> = balances
-            .into_iter()
-            .filter(|(_, balance)| balance > &U256::from(BALANCE_THRESHOLD))
-            .collect();
+        let filtered_balances: Vec<(H160, U256)> =
+            filter_address_on_balance(balances, BALANCE_THRESHOLD);
 
-        println!("Balances with balance > 0 eth: {:?}", filtered_balances);
+        println!("Balances with balance >= 0 eth: {:?}", filtered_balances);
     }
 
     Some(())
@@ -86,6 +57,49 @@ pub async fn get_balance_address(
     Ok((from, balance))
 }
 
+pub async fn get_transactions(
+    ws_provider: &Provider<Ws>,
+    transactions_hashes: Vec<H256>,
+) -> Vec<Option<Transaction>> {
+    let transactions = future::try_join_all(
+        transactions_hashes
+            .iter()
+            .map(|tx_hash| ws_provider.get_transaction(*tx_hash)),
+    )
+    .await
+    .unwrap();
+
+    transactions
+}
+
+pub fn filter_contract_creation(transactions: Vec<Option<Transaction>>) -> Vec<Transaction> {
+    transactions
+        .into_iter()
+        .filter(|t| t.is_some())
+        .map(|t| t.unwrap())
+        .filter(|t| t.to.is_none())
+        .collect()
+}
+
+pub async fn get_contract_addresses(
+    ws_provider: &Provider<Ws>,
+    transactions: Vec<Transaction>,
+) -> Vec<H160> {
+    let tx_receipts = future::try_join_all(
+        transactions
+            .iter()
+            .map(|tx| ws_provider.get_transaction_receipt(tx.hash)),
+    )
+    .await
+    .unwrap();
+
+    tx_receipts
+        .into_iter()
+        .filter(|tx_receipts| tx_receipts.is_some())
+        .map(|tx_receipts| tx_receipts.unwrap().contract_address.unwrap())
+        .collect()
+}
+
 // getting a list of all tx from a specific block
 pub async fn get_all_tx_from_block(
     ws_provider: &Provider<Ws>,
@@ -93,6 +107,30 @@ pub async fn get_all_tx_from_block(
 ) -> Result<Vec<TxHash>, Box<dyn Error>> {
     let block = ws_provider.get_block(block_hash).await?.unwrap();
     Ok(block.transactions)
+}
+
+pub async fn get_balances(
+    ws_provider: &Provider<Ws>,
+    addresses: Vec<H160>,
+    block: H256,
+) -> Vec<(H160, U256)> {
+    future::try_join_all(
+        addresses
+            .iter()
+            .map(|addr| get_balance_address(&ws_provider, *addr, block)),
+    )
+    .await
+    .unwrap()
+}
+
+pub fn filter_address_on_balance(
+    addresses_to_balance: Vec<(H160, U256)>,
+    balance_threshold: u8,
+) -> Vec<(H160, U256)> {
+    addresses_to_balance
+        .into_iter()
+        .filter(|(_, balance)| balance >= &U256::from(balance_threshold))
+        .collect()
 }
 
 #[tokio::main]
